9de20dd64e6f127955a7060e172382c378a2577f
Zaid Hasan <zaid.hasan@primathon.in>
2023-07-17 21:09:11 +0530

    Added: Create secondary contact functionality

--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { LinkPrecedenceType } from 'src/utils/enums';
+import {
+  findAndUnshiftElement,
+  getUniqueValuesFromObjectArray,
+} from 'src/utils/utils';
 import { IsNull, Repository } from 'typeorm';
+import { IdentifyContactResponseDto } from './dto/identify-contact-response.dto';
 import { IdentifyContactDto } from './dto/identify-contact.dto';
 import { Contact } from './entities/contact.entity';
-import { IdentifyContactResponseDto } from './dto/identify-contact-response.dto';
 
 @Injectable()
 export class ContactsService {
@@ -29,50 +34,124 @@ export class ContactsService {
     });
   }
 
-  create(contact: Contact) {
+  saveContact(contact: Contact) {
     return this.contactRepository.save(contact);
+  }
+
+  getPrimaryContact(contacts: Contact[]) {
+    return contacts.filter(
+      (contact) => contact.linkPrecedence === LinkPrecedenceType.Primary,
+    );
   }
 
   async identify(
     identifyContactDto: IdentifyContactDto,
   ): Promise<IdentifyContactResponseDto> {
     try {
-      let contactsByEmailPromise: Promise<Contact[]>;
-      let contactsByPhonePromise: Promise<Contact[]>;
-      if (identifyContactDto.email) {
-        contactsByEmailPromise = this.findByEmail(identifyContactDto.email);
-      }
-      if (identifyContactDto.phoneNumber) {
-        contactsByPhonePromise = this.findByPhone(
-          identifyContactDto.phoneNumber,
-        );
-      }
+      const contactsByEmailPromise = identifyContactDto.email
+        ? this.findByEmail(identifyContactDto.email)
+        : Promise.resolve([]);
+      const contactsByPhonePromise = identifyContactDto.phoneNumber
+        ? this.findByPhone(identifyContactDto.phoneNumber)
+        : Promise.resolve([]);
+
       const [contactsByEmail, contactsByPhone] = await Promise.all([
         contactsByEmailPromise,
         contactsByPhonePromise,
       ]);
+
       const hasContactsByEmail = contactsByEmail.length;
       const hasContactsByPhone = contactsByPhone.length;
-      /**
-       * If no contact is found using email and phone, then create a new contact
-       */
-      let identifiedContact: IdentifyContactResponseDto;
+
       if (!hasContactsByEmail && !hasContactsByPhone) {
-        const primaryContactPayload = this.contactRepository.create({
-          email: identifyContactDto.email,
-          phoneNumber: identifyContactDto.phoneNumber,
-        });
-        const primaryContact = await this.create(primaryContactPayload);
-        identifiedContact = {
-          primaryContatctId: primaryContact.id,
-          emails: primaryContact.email ? [primaryContact.email] : [],
-          phoneNumbers: primaryContact.phoneNumber
-            ? [primaryContact.phoneNumber]
-            : [],
-          secondaryContactIds: [],
-        };
-        return identifiedContact;
-      } 
-    } catch (err) {}
+        return this.createPrimaryContact(identifyContactDto);
+      } else if (
+        (hasContactsByEmail &&
+          !hasContactsByPhone &&
+          identifyContactDto.phoneNumber) ||
+        (hasContactsByPhone && !hasContactsByEmail && identifyContactDto.email)
+      ) {
+        return this.createSecondaryContact(
+          identifyContactDto,
+          contactsByEmail,
+          contactsByPhone,
+        );
+      }
+    } catch (err) {
+      // Handle the error appropriately
+    }
+  }
+
+  private async createPrimaryContact(
+    identifyContactDto: IdentifyContactDto,
+  ): Promise<IdentifyContactResponseDto> {
+    const primaryContactPayload = this.contactRepository.create({
+      email: identifyContactDto.email,
+      phoneNumber: identifyContactDto.phoneNumber,
+    });
+    const primaryContact = await this.saveContact(primaryContactPayload);
+
+    const identifiedContact: IdentifyContactResponseDto = {
+      primaryContatctId: primaryContact.id,
+      emails: primaryContact.email ? [primaryContact.email] : [],
+      phoneNumbers: primaryContact.phoneNumber
+        ? [primaryContact.phoneNumber]
+        : [],
+      secondaryContactIds: [],
+    };
+
+    return identifiedContact;
+  }
+
+  private async createSecondaryContact(
+    identifyContactDto: IdentifyContactDto,
+    contactsByEmail: Contact[],
+    contactsByPhone: Contact[],
+  ): Promise<IdentifyContactResponseDto> {
+    const existingContacts = contactsByEmail.length
+      ? contactsByEmail
+      : contactsByPhone;
+
+    const primaryContact = this.getPrimaryContact(existingContacts);
+
+    const secondaryContactPayload = this.contactRepository.create({
+      email: identifyContactDto.email,
+      phoneNumber: identifyContactDto.phoneNumber,
+      linked: primaryContact[0],
+      linkPrecedence: LinkPrecedenceType.Secondary,
+    });
+    const secondaryContact = await this.saveContact(secondaryContactPayload);
+
+    let emails = [];
+    let phoneNumbers = [];
+    let secondaryContactIds = getUniqueValuesFromObjectArray(
+      [...contactsByEmail, ...contactsByPhone],
+      'id',
+    );
+    secondaryContactIds.splice(
+      secondaryContactIds.indexOf(primaryContact[0]['id']),
+      1,
+    );
+
+    if (contactsByEmail.length) {
+      emails = getUniqueValuesFromObjectArray(contactsByEmail, 'email');
+      findAndUnshiftElement(emails, primaryContact[0]['email']);
+    }
+    if (contactsByPhone.length) {
+      phoneNumbers = getUniqueValuesFromObjectArray(
+        contactsByPhone,
+        'phoneNumber',
+      );
+      findAndUnshiftElement(phoneNumbers, primaryContact[0]['phoneNumber']);
+    }
+
+    const identifiedContact: IdentifyContactResponseDto = {
+      primaryContatctId: primaryContact[0]['id'],
+      emails: [...emails, identifyContactDto.email],
+      phoneNumbers: [...phoneNumbers, identifyContactDto.phoneNumber],
+      secondaryContactIds: [...secondaryContactIds, secondaryContact.id],
+    };
+
+    return identifiedContact;
   }
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,15 @@
 export function toBool(value: string): boolean {
   return value === 'true';
 }
+
+export function getUniqueValuesFromObjectArray(array: any[], key: string) {
+  let valuesArray = [];
+  array.forEach((value) => {
+    valuesArray.push(value[key]);
+  });
+  return Array.from(new Set(valuesArray));
+}
+
+export function findAndUnshiftElement(array: any[], value: any) {
+  return array.unshift(array.splice(array.indexOf(value), 1)[0]);
+}
